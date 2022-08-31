bd87fe71d6acd67d24d91871ef068f5e41e2490d
Benson <bensonmakau2000@gmail.com>
2022-08-31 13:12:11 +0300

    event loop in practice

---
+++ event-loop.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", "utf8", () => {
+  console.log("I/O FINISHED");
+  console.log("********************");
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finished"));
+
+  process.nextTick(() => console.log("Process.nextTick"));
+});
+
+console.log("Hello From the top level code");
